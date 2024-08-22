188f0412c6998176663183fc1ef2e0d759cf2e87
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-08-22 13:40:42 -0500

    add category checkbox

--- Shopping-Cart/src/routes/shopPage.jsx
+++ Shopping-Cart/src/routes/shopPage.jsx
@@ -3,8 +3,26 @@ import "../styles/pageTemplate.css";
 import logoImg from "../assets/logo.svg";
 import cartImg from "../assets/shopping-cart-logo.svg";
 import style from "../styles/Button.module.css";
+import "../styles/shopPage.css";
+import books from "../assets/book_data.json";
 
 export default function ShopPage() {
+  const categories = () => {
+    const list = [];
+    for (const [key, book] of Object.entries(books.books)) {
+      console.log(`Processing book with ID: ${key}`, book);
+      const genres = book["genres"];
+      genres.forEach((genre) => {
+        if (genre && !list.includes(genre)) {
+          list.push(genre);
+        }
+      });
+    }
+    return list;
+  };
+
+  const genreList = categories().sort();
+
   return (
     <>
       <header>
@@ -15,11 +33,28 @@ export default function ShopPage() {
           <Link to="/">Home</Link>
         </div>
         <Link id="cartLogo" className={style.button} to="/cart">
-          <img src={cartImg} />
+          <img src={cartImg} alt="Cart" />
         </Link>
       </header>
       <div id="content">
-        <div id="sidebar"></div>
+        <div id="sidebar">
+          <div id="search-bar">
+            <label>Search:</label>
+            <input type="text" />
+          </div>
+          <hr />
+          <div id="categories">
+            <h2>Categories</h2>
+            <ul>
+              {genreList.map((genre, id) => (
+                <li key={id}>
+                  <input type="checkbox" id={`checkbox-${id}`}></input>
+                  <label htmlFor={`checkbox-${id}`}>{genre}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
         <div id="main"></div>
       </div>
       <footer></footer>
