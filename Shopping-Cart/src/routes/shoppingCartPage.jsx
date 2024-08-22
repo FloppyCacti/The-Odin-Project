70ead322a4c2bcfce72375e7cfbab622721e8cd3
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-08-22 11:56:39 -0500

    make basic page template

--- Shopping-Cart/src/routes/shoppingCartPage.jsx
+++ Shopping-Cart/src/routes/shoppingCartPage.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
+import "../styles/pageTemplate.css";
+import style from "../styles/Button.module.css";
+import logoImg from "../assets/logo.svg";
+import cartImg from "../assets/shopping-cart-logo.svg";
+
 export default function ShoppingCartPage() {
   return (
     <>
-      <div>
-        <h1>test</h1>
+      <header>
+        <div id="homeButton">
+          <Link id="logo" className={style.button} to="/">
+            <img src={logoImg} alt="Logo" />
+          </Link>
+          <Link to="/">Home</Link>
+        </div>
+        <Link id="cartLogo" className={style.button} to="/cart">
+          <img src={cartImg} />
+        </Link>
+      </header>
+      <div id="content">
+        <div id="sidebar"></div>
+        <div id="main"></div>
       </div>
+      <footer></footer>
     </>
   );
 }
