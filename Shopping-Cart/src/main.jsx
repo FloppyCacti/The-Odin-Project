33efc8cc68e66d3b3545f6d5625144958330da50
Parth <116512352+FloppyCacti@users.noreply.github.com>
2024-08-28 13:56:37 -0500

    nest routerProver in CartProvider

--- Shopping-Cart/src/main.jsx
+++ Shopping-Cart/src/main.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
+import { CartProvider } from "./CartContext.jsx";
 import "./index.css";
 import routes from "./routes";
 
@@ -8,6 +9,8 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </StrictMode>
 );
